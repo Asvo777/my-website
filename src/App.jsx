74236e8849567f4bad0209d9de266f45a66612ac
Évence Batobo <126938639+Asvo777@users.noreply.github.com>
2025-01-13 00:01:32 -0500

    Update BtnSvg

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,17 @@ const App = () => {
   const [darkMode, setDarkMode] = useState(false);
 
   return (
-    <div className={darkMode ? "dark pt-10 pb-10" : " pt-10 pb-10"}>
-      <button className="btn-primary" onClick={() => setDarkMode(!darkMode)}>
-        Change theme
-      </button>
+    <>
     <h1 className="text-3xl font-bold underline">
       Hello World !
     </h1>
     <div className="mt=10 pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden" href="login">
-      <Button className={"mt-10"} px={"px-7"} white>
-        LoL  
+      <Button className="mt-10">
+        Thanks  
       </Button>
     </div>
       < ButtonGradient/>
-    </div>
+    </>
   );
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,16 @@
 import BtnSvg from '../assets/svg/BtnSvg'
 
 const Button = ({ className, href, onClick, children, px, white}) => {
-  const classes = 'button relative inline-flex items-center justify-center h-11 transition-color border hover:text-color-1 ${px || "px-7"} ${white ? "text-n-8" : "text-n-1"} ${className || ""}}';
+  const classes = 'button relative inline-flex items-center justify-center h-11 transition-color hover:text-color-1 ${px || "px-7"} ${white ? "text-n-8" : "text-n-1"} ${className || ""}}';
   
-  const renderBtn = () => {
-    return (
-    <button classNAme={classes}>
+  const renderBtn = () => (
+    <button className={classes}>
       <span>{children}</span>
       {BtnSvg(white)}
     </button>
   )
-}
+
+  return renderBtn();
 }
 
 export default Button;
